548ce724c3a416780e2155704bdfae50cb08530f
Friedrich Stein <j.friedrich-stein@gmx.de>
2022-05-02 23:04:02 +0200

    script finished and seems to be working

--- main.py
+++ main.py
@@ -1,2 +1,25 @@
+import sys
+import csv
+
+from bs4 import BeautifulSoup
+
 if __name__ == '__main__':
-    pass
+    file = sys.argv[1]
+    with open(file) as fp:
+        soup = BeautifulSoup(fp, 'html.parser')
+    author = soup.findAll('div', {'class': 'bookmark'})[1].getText(strip=True)
+    title = soup.findAll('div', {'class': 'bookmark'})[0].getText(strip=True)
+    title_index = title.find(' - ') + 3
+    title = title[title_index:]
+    with open(f'{title} - {author}.csv', 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile, delimiter=',')
+        writer.writerow(('Highlight','Title', 'Author', 'Note', 'Location'))
+        for element in soup.findAll('div', {'class': 'bookmark', 'id': True}):
+            page = element.findNext('p', {'class': 'bm-page'}).string
+            highlight = element.findNext('div', {'class': 'bm-text'}).p.getText('\n')
+            note = element.findNext('div', {'class': 'bm-note'})
+            if note is not None:
+                note_text = note.p.getText('\n')
+            else:
+                note_text = ''
+            writer.writerow((highlight, title, author, note_text, page))
